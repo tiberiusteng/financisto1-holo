bf2d8a972b60aea53769d8fafec4bbbb045c4083
Tib Teng <teng@tib.tw>
2018-10-10 17:26:20 +0900

    Revert "Fingerprint unlock"
    This reverts commit a571797334125aa87536bf633e3ec8ac3751f2c2.

--- app/src/main/java/ru/orangesoftware/financisto/activity/PinActivity.java
+++ app/src/main/java/ru/orangesoftware/financisto/activity/PinActivity.java
@@ -14,56 +14,22 @@ import ru.orangesoftware.financisto.R;
 import ru.orangesoftware.financisto.utils.MyPreferences;
 import ru.orangesoftware.financisto.utils.PinProtection;
 import ru.orangesoftware.financisto.view.PinView;
-
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.hardware.fingerprint.FingerprintManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.CancellationSignal;
 
 public class PinActivity extends Activity implements PinView.PinListener {
 	
 	public static final String SUCCESS = "PIN_SUCCESS";
 
-	private FingerprintManager fingerprintManager;
-	private CancellationSignal cancelFingerprintCheck;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		String pin = MyPreferences.getPin(this);
-
 		if (pin == null) {
 			onSuccess(null);
 		} else {
 			PinView v = new PinView(this, this, pin, R.layout.lock);
-
-			if (Build.VERSION.SDK_INT >= 23) {
-				class FingerprintCallback extends FingerprintManager.AuthenticationCallback {
-					private PinView.PinListener listener;
-					public FingerprintCallback(PinView.PinListener pListener) {
-						this.listener = pListener;
-					}
-					@Override
-					public void onAuthenticationSucceeded(
-							FingerprintManager.AuthenticationResult result)
-					{
-						this.listener.onSuccess(null);
-					}
-				}
-
-				fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
-				cancelFingerprintCheck = new CancellationSignal();
-
-				if ((checkSelfPermission(Manifest.permission.USE_FINGERPRINT) == PackageManager.PERMISSION_GRANTED) &&
-						fingerprintManager.isHardwareDetected() && fingerprintManager.hasEnrolledFingerprints()) {
-					fingerprintManager.authenticate(null, cancelFingerprintCheck, 0, new FingerprintCallback(this), null);
-				}
-			}
-
 			setContentView(v.getView());
 		}
 	}
@@ -74,7 +40,6 @@ public class PinActivity extends Activity implements PinView.PinListener {
 
 	@Override
 	public void onSuccess(String pinBase64) {
-		if (cancelFingerprintCheck != null) cancelFingerprintCheck.cancel();
         PinProtection.pinUnlock(this);
 		Intent data = new Intent();
 		data.putExtra(SUCCESS, true);
@@ -84,7 +49,6 @@ public class PinActivity extends Activity implements PinView.PinListener {
 
 	@Override
 	public void onBackPressed() {
-		if (cancelFingerprintCheck != null) cancelFingerprintCheck.cancel();
         moveTaskToBack(true);
 	}
 
