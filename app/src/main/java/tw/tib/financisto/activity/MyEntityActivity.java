b7cfd458d2f3b244928c6a2182e302a7249e5c42
Tib Teng <661892+tiberiusteng@users.noreply.github.com>
2023-11-23 01:31:11 +0900

    use dedicated view for creating entity without unused active checkbox

--- app/src/main/java/tw/tib/financisto/activity/MyEntityActivity.java
+++ app/src/main/java/tw/tib/financisto/activity/MyEntityActivity.java
@@ -51,7 +51,7 @@ public abstract class MyEntityActivity<T extends MyEntity> extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.project);
+		setContentView(R.layout.entity_title);
 
 		db = new DatabaseAdapter(this);
 		db.open();
