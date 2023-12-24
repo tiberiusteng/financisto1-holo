539c050ca174044d30cd8b55f288aae7c1b6e63d
Tib Teng <661892+tiberiusteng@users.noreply.github.com>
2023-12-25 00:36:02 +0900

    add synchronized for criterias and sorts
    better protect WhereFilter for loader execution in background thread

--- app/src/main/java/tw/tib/financisto/filter/WhereFilter.java
+++ app/src/main/java/tw/tib/financisto/filter/WhereFilter.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -49,62 +50,62 @@ public class WhereFilter {
 		this.title = title;
 	}
 
-	public WhereFilter eq(Criteria c) {
+	public synchronized WhereFilter eq(Criteria c) {
 		criterias.add(c);
 		return this;
 	}
 
-	public WhereFilter eq(String column, String value) {
+	public synchronized WhereFilter eq(String column, String value) {
 		criterias.add(Criteria.eq(column, value));
 		return this;
 	}
 
-	public WhereFilter neq(String column, String value) {
+	public synchronized WhereFilter neq(String column, String value) {
 		criterias.add(Criteria.neq(column, value));
 		return this;
 	}
 
-	public WhereFilter btw(String column, String value1, String value2) {
+	public synchronized WhereFilter btw(String column, String value1, String value2) {
 		criterias.add(Criteria.btw(column, value1, value2));
 		return this;
 	}
 
-	public WhereFilter gt(String column, String value) {
+	public synchronized WhereFilter gt(String column, String value) {
 		criterias.add(Criteria.gt(column, value));
 		return this;
 	}
 
-	public WhereFilter gte(String column, String value) {
+	public synchronized WhereFilter gte(String column, String value) {
 		criterias.add(Criteria.gte(column, value));
 		return this;
 	}
 
-	public WhereFilter lt(String column, String value) {
+	public synchronized WhereFilter lt(String column, String value) {
 		criterias.add(Criteria.lt(column, value));
 		return this;
 	}
 
-	public WhereFilter lte(String column, String value) {
+	public synchronized WhereFilter lte(String column, String value) {
 		criterias.add(Criteria.lte(column, value));
 		return this;
 	}
 
-	public WhereFilter isNull(String column) {
+	public synchronized WhereFilter isNull(String column) {
 		criterias.add(Criteria.isNull(column));
 		return this;
 	}
 
-	public WhereFilter asc(String column) {
+	public synchronized WhereFilter asc(String column) {
 		sorts.add(column+" asc");
 		return this;
 	}
 
-	public WhereFilter desc(String column) {
+	public synchronized WhereFilter desc(String column) {
 		sorts.add(column+" desc");
 		return this;
 	}
 
-	public WhereFilter contains(String column, String text){
+	public synchronized WhereFilter contains(String column, String text){
 		criterias.add(Criteria.like(column, String.format("%%%s%%", text)));
 		return this;
 	}
@@ -128,7 +129,7 @@ public class WhereFilter {
 		return args;
 	}
 
-	public Criteria get(String name) {
+	public synchronized Criteria get(String name) {
 		for (Criteria c : criterias) {
 			String column = c.columnName;
 			if (name.equals(column)) {
@@ -142,7 +143,7 @@ public class WhereFilter {
 		return (DateTimeCriteria)get(BlotterFilter.DATETIME);
 	}
 
-	public Criteria put(Criteria criteria) {
+	public synchronized Criteria put(Criteria criteria) {
 		for (int i=0; i<criterias.size(); i++) {
 			Criteria c = criterias.get(i);
 			if (criteria.columnName.equals(c.columnName)) {
@@ -154,7 +155,7 @@ public class WhereFilter {
 		return null;
 	}
 
-	public Criteria remove(String name) {
+	public synchronized Criteria remove(String name) {
 		for (Iterator<Criteria> i = criterias.iterator(); i.hasNext();) {
 			Criteria c = i.next();
 			if (name.equals(c.columnName)) {
@@ -165,23 +166,25 @@ public class WhereFilter {
 		return null;
 	}
 
-	public void clear() {
+	public synchronized void clear() {
 		criterias.clear();
 		sorts.clear();
 	}
 
 	public static WhereFilter copyOf(WhereFilter filter) {
-		WhereFilter f = new WhereFilter(filter.title);
-		f.criterias.addAll(filter.criterias);
-		f.sorts.addAll(filter.sorts);
-		return f;
+		synchronized (filter) {
+			WhereFilter f = new WhereFilter(filter.title);
+			f.criterias.addAll(filter.criterias);
+			f.sorts.addAll(filter.sorts);
+			return f;
+		}
 	}
 
 	public static WhereFilter empty() {
 		return new WhereFilter("");
 	}
 
-	public Expression toWhereExpression() {
+	public synchronized Expression toWhereExpression() {
 		int count = criterias.size();
 		Expression[] ee = new Expression[count];
 		for (int i=0; i<count; i++) {
@@ -190,7 +193,7 @@ public class WhereFilter {
 		return Expressions.and(ee);
 	}
 
-	public void toBundle(Bundle bundle) {
+	public synchronized void toBundle(Bundle bundle) {
 		String[] extras = new String[criterias.size()];
 		for (int i=0; i<extras.length; i++) {
 			extras[i] = criterias.get(i).toStringExtra();
@@ -203,17 +206,19 @@ public class WhereFilter {
 	public static WhereFilter fromBundle(Bundle bundle) {
 		String title = bundle.getString(TITLE_EXTRA);
 		WhereFilter filter = new WhereFilter(title);
-		String[] a = bundle.getStringArray(FILTER_EXTRA);
-		if (a != null) {
-			for (String s : a) {
-				filter.put(Criteria.fromStringExtra(s));
+		synchronized (filter) {
+			String[] a = bundle.getStringArray(FILTER_EXTRA);
+			if (a != null) {
+				for (String s : a) {
+					filter.put(Criteria.fromStringExtra(s));
+				}
 			}
-		}
-		String sortOrder = bundle.getString(SORT_ORDER_EXTRA);
-		if (sortOrder != null) {
-			String[] orders = sortOrder.split(",");
-			if (orders != null && orders.length > 0) {
-				filter.sorts.addAll(Arrays.asList(orders));
+			String sortOrder = bundle.getString(SORT_ORDER_EXTRA);
+			if (sortOrder != null) {
+				String[] orders = sortOrder.split(",");
+				if (orders != null && orders.length > 0) {
+					filter.sorts.addAll(Arrays.asList(orders));
+				}
 			}
 		}
 		return filter;
@@ -232,7 +237,7 @@ public class WhereFilter {
 		return fromBundle(bundle);
 	}
 
-	public String getSortOrder() {
+	public synchronized String getSortOrder() {
 		StringBuilder sb = new StringBuilder();
 		for (String o : sorts) {
 			if (sb.length() > 0) {
@@ -243,11 +248,11 @@ public class WhereFilter {
 		return sb.toString();
 	}
 
-	public void resetSort() {
+	public synchronized void resetSort() {
 		sorts.clear();
 	}
 
-	public void toSharedPreferences(SharedPreferences preferences) {
+	public synchronized void toSharedPreferences(SharedPreferences preferences) {
 		Editor e = preferences.edit();
 		int count = criterias.size();
 		e.putString(FILTER_TITLE_PREF, title);
@@ -262,29 +267,35 @@ public class WhereFilter {
 	public static WhereFilter fromSharedPreferences(SharedPreferences preferences) {
 		String title = preferences.getString(FILTER_TITLE_PREF, "");
 		WhereFilter filter = new WhereFilter(title);
-		int count = preferences.getInt(FILTER_LENGTH_PREF, 0);
-		if (count > 0) {
-			for (int i=0; i<count; i++) {
-				String criteria = preferences.getString(FILTER_CRITERIA_PREF+i, "");
-				if (criteria.length() > 0) {
-					filter.put(Criteria.fromStringExtra(criteria));
+		synchronized (filter) {
+			int count = preferences.getInt(FILTER_LENGTH_PREF, 0);
+			if (count > 0) {
+				for (int i = 0; i < count; i++) {
+					String criteria = preferences.getString(FILTER_CRITERIA_PREF + i, "");
+					if (criteria.length() > 0) {
+						filter.put(Criteria.fromStringExtra(criteria));
+					}
 				}
 			}
-		}
-		String sortOrder = preferences.getString(FILTER_SORT_ORDER_PREF, "");
-		String[] orders = sortOrder.split(",");
-		if (orders != null && orders.length > 0) {
-			filter.sorts.addAll(Arrays.asList(orders));
+			String sortOrder = preferences.getString(FILTER_SORT_ORDER_PREF, "");
+			String[] orders = sortOrder.split(",");
+			if (orders != null && orders.length > 0) {
+				filter.sorts.addAll(Arrays.asList(orders));
+			}
 		}
 		return filter;
 	}
 
-	public String getSelection() {
-		return getSelection(criterias);
+	public synchronized String getSelection() {
+		String ret = getSelection(criterias);
+		Log.d("WhereFilter", "getSelection=" + ret);
+		return ret;
 	}
 
-	public String[] getSelectionArgs() {
-		return getSelectionArgs(criterias);
+	public synchronized String[] getSelectionArgs() {
+		String[] ret = getSelectionArgs(criterias);
+		Log.d("WhereFilter", "getSelectionArgs=" + String.join(",", ret));
+		return ret;
 	}
 
 	public long getAccountId() {
@@ -316,7 +327,7 @@ public class WhereFilter {
 		return title;
 	}
 
-	public boolean isEmpty() {
+	public synchronized boolean isEmpty() {
 		return criterias.isEmpty();
 	}
 
