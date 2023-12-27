9c6ad752812af6a02d0cf675b25f23ad4a964720
Tib Teng <661892+tiberiusteng@users.noreply.github.com>
2023-12-27 17:42:59 +0900

    transaction status icon/color sync

--- app/src/main/java/tw/tib/financisto/model/TransactionStatus.java
+++ app/src/main/java/tw/tib/financisto/model/TransactionStatus.java
@@ -15,10 +15,10 @@ import tw.tib.financisto.utils.LocalizableEnum;
 
 public enum TransactionStatus implements LocalizableEnum {
 	RS(R.string.transaction_status_restored, R.drawable.transaction_status_restored_2, R.color.restored_transaction_color),
-	PN(R.string.transaction_status_pending, R.drawable.transaction_status_pending_2, R.color.pending_transaction_color),
+	PN(R.string.transaction_status_pending, R.drawable.transaction_status_pending_3, R.color.pending_transaction_color),
 	UR(R.string.transaction_status_unreconciled, R.drawable.transaction_status_unreconciled_2, R.color.unreconciled_transaction_color),
-	CL(R.string.transaction_status_cleared, R.drawable.transaction_status_cleared_2, R.color.cleared_transaction_color),
-	RC(R.string.transaction_status_reconciled, R.drawable.transaction_status_reconciled_2, R.color.reconciled_transaction_color);
+	CL(R.string.transaction_status_cleared, R.drawable.transaction_status_cleared_3, R.color.cleared_transaction_color),
+	RC(R.string.transaction_status_reconciled, R.drawable.transaction_status_reconciled_3, R.color.reconciled_transaction_color);
 	
 	public final int titleId;
 	public final int iconId;
