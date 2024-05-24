8f22bfc0e239b3f6e99fccc430019739cb981f0e
Tib Teng <661892+tiberiusteng@users.noreply.github.com>
2024-05-25 02:56:00 +0900

    update last used category/location/project when editing transaction

--- app/src/main/java/tw/tib/financisto/db/DatabaseAdapter.java
+++ app/src/main/java/tw/tib/financisto/db/DatabaseAdapter.java
@@ -494,6 +494,7 @@ public class DatabaseAdapter extends MyEntityManager {
             updateAccountBalance(oldT.fromAccountId, oldT.fromAmount, t.fromAccountId, t.fromAmount);
             updateAccountBalance(oldT.toAccountId, oldT.toAmount, t.toAccountId, t.toAmount);
             updateRunningBalance(oldT, t);
+            updateLastUsed(t);
             if (oldT.locationId != t.locationId) {
                 updateLocationCount(oldT.locationId, -1);
                 updateLocationCount(t.locationId, 1);
