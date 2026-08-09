9154dbcdcc589e3f38efa200a0eaaf2e2296062f
Tib Teng <661892+tiberiusteng@users.noreply.github.com>
2026-08-09 13:58:59 +0800

    only update project, location if selector is shown
    adapted from https://github.com/mpstudios56/cifra-android/commit/7cab11de5b0939b452dbf8e694db375ffb1e0a1a

--- app/src/main/java/tw/tib/financisto/activity/AbstractTransactionActivity.java
+++ app/src/main/java/tw/tib/financisto/activity/AbstractTransactionActivity.java
@@ -816,8 +816,12 @@ public abstract class AbstractTransactionActivity extends AbstractActivity imple
 
 	protected void updateTransactionFromUI(Transaction transaction) {
 		transaction.categoryId = categorySelector.getSelectedCategoryId();
-		transaction.projectId = projectSelector.getSelectedEntityId();
-		transaction.locationId = locationSelector.getSelectedEntityId();
+		if (isShowProject) {
+			transaction.projectId = projectSelector.getSelectedEntityId();
+		}
+		if (isShowLocation) {
+			transaction.locationId = locationSelector.getSelectedEntityId();
+		}
 		if (transaction.isScheduled()) {
 			DateUtils.zeroSeconds(dateTime);
 		}
