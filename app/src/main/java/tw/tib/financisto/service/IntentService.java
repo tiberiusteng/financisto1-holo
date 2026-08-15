8bb42d936b8d31f22009a9b7ff26de7493d9a516
Tib Teng <661892+tiberiusteng@users.noreply.github.com>
2026-08-16 02:14:52 +0800

    change IntentService to base on android.app.IntentService; support intent-triggered create backup
    #141

--- app/src/main/java/tw/tib/financisto/service/IntentService.java
+++ app/src/main/java/tw/tib/financisto/service/IntentService.java
@@ -1,22 +1,26 @@
 package tw.tib.financisto.service;
 
 import android.app.Notification;
-import android.app.Service;
 import android.content.Intent;
-import android.os.IBinder;
 
 import tw.tib.financisto.R;
 import tw.tib.financisto.activity.AccountWidget;
+import tw.tib.financisto.backup.DatabaseExport;
 import tw.tib.financisto.db.DatabaseAdapter;
 import tw.tib.financisto.model.Transaction;
 import tw.tib.financisto.model.TransactionInfo;
 import tw.tib.financisto.utils.NotificationUtils;
 
-public class IntentService extends Service {
+public class IntentService extends android.app.IntentService {
     public static final String ACTION_NEW_TRANSACTION = "tw.tib.financisto.NEW_TRANSACTION";
+    public static final String ACTION_CREATE_BACKUP = "tw.tib.financisto.CREATE_BACKUP";
 
     private DatabaseAdapter db;
     private IntentTransactionProcessor intentTransactionProcessor;
+
+    public IntentService() {
+        super("IntentService");
+    }
 
     @Override
     public void onCreate() {
@@ -26,21 +30,24 @@ public class IntentService extends Service {
         intentTransactionProcessor = new IntentTransactionProcessor(db);
     }
 
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public void onHandleIntent(Intent intent) {
         if (intent != null) {
             String action = intent.getAction();
             if (action != null) {
                 switch (action) {
                     case ACTION_NEW_TRANSACTION -> createTransactionFromIntent(intent);
+                    case ACTION_CREATE_BACKUP -> createBackup();
                 }
             }
         }
-        return START_STICKY;
+    }
+
+    private void createBackup() {
+        try {
+            new DatabaseExport(this, db.db(), true).export();
+        } catch (Exception ignore) {
+        }
     }
 
     private void createTransactionFromIntent(Intent intent) {
